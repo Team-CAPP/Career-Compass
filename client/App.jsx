b2cbc85cf6e0ee205a6d33c4db6a11ca88b2b364
Justin Chu <justinchu24.com>
2023-10-16 17:29:58 -0400

    pushing frontend setup

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import AppForm from './components/AppForm';
+
+
+function App() {
+  return (
+      <div className='App'>
+        <Navbar> </Navbar>
+        <Intro> </Intro>
+        <Gallery> </Gallery>
+        <Footer></Footer>
+      </div>
+  );
+}
+
+export default App;
---
+++ client/components/AppForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppForm() {
+
+  return (
+    <></>
+  )
+
+}
+
+export default AppForm;
