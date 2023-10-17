4bc5580cc97b80f0e7d00dd58d6fdb3a669de0d1
Justin Chu <justinchu24.com>
2023-10-17 01:47:10 -0400

    making initial set up for tomorrow.  Login page

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,7 @@ import Notes from './components/Notes';
 
 function App() {
   return (
+    //needs to implement React router to serve login then main application. And Form on button click
     <div className='App'>
       <div className='Form-Container'>
         <AppForm></AppForm>
