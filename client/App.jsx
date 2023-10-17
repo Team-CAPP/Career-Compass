25ade4817167b15b7c875498f03c9227fba9aef1
Justin Chu <justinchu24.com>
2023-10-17 15:59:22 -0400

    commiting to save changes

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppForm from './components/AppForm';
 import Notes from './components/Notes';
+import Login from './components/Login';
 
 function App() {
   return (
---
+++ client/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className='Login-Container'>
+      
+    </div>
+  );
+}
+
+export default Login;
