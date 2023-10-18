ff5ba82702a8f61805d79e685f7429675ab4abfb
Rachel Power <96928310+rpower15@users.noreply.github.com>
2023-10-18 09:03:43 -0700

    Fixed routes for default to login

--- client/pages/Application.jsx
+++ client/pages/Application.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AppForm from '../components/AppForm';
 import Notes from '../components/Notes';
+import Navbar from '../components/Navbar';
 
 function Application() {
   return (
     <>
+      <div className='navBar'>
+        <Navbar />
+      </div>
       <div className='Form-Container'>
         <AppForm />
         <Notes />
